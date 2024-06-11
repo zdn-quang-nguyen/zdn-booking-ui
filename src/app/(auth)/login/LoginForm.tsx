c7e12a0436665c6351eb932618b7aa36f9455fd7
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-11 13:36:56 +0700

    feat: add sign up ui
    Co-authored-by: zdn-duc-nguyen <duc.nguyen@zodinet.tech>

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,28 +1,30 @@
-'use client';
-import { loginSchema } from '@/zod-schemas/login-schema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import style from './login.module.scss';
-import { cn } from '@/libs/utils';
+"use client";
+import { loginSchema } from "@/zod-schemas/login-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import style from "./login.module.scss";
+import { cn } from "@/libs/utils";
 
 type FormData = z.infer<typeof loginSchema>;
 
 export default function LoginForm() {
-    const {
-        handleSubmit,
-        register,
-        getValues,
-        formState: { errors, isSubmitting, isDirty, isValid },
-    } = useForm<FormData>({
-        resolver: zodResolver(loginSchema),
-    });
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors, isSubmitting, isDirty, isValid },
+  } = useForm<FormData>({
+    resolver: zodResolver(loginSchema),
+  });
 
-    async function onSubmit(data: FormData) {
-        console.log(isSubmitting);
-        console.log(data);
-    }
+  async function onSubmit(data: FormData) {
+    console.log(isSubmitting);
+    console.log(data);
+  }
 
-    return <div className="selection:bg-rose-500 selection:text-white">cascsacsa</div>;
+  return (
+    <div className="selection:bg-rose-500 selection:text-white">cascsacsa</div>
+  );
 }
