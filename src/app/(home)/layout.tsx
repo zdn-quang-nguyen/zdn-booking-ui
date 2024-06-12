bdca6a6e6999e3ef61761d1950a265fab5ef2bb2
zdn-quang-nguyen <quang.nguyen@zodinet.tech>
2024-06-12 09:21:22 +0700

    feat: Add footer layout and update home layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,14 +1,16 @@
+import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 
 type HomeLayoutProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 export default function HomeLayout({ children }: HomeLayoutProps) {
-  return (
-    <main>
-      <Header />
-      {children}
-    </main>
-  );
+    return (
+        <main>
+            <Header />
+            {children}
+            <Footer />
+        </main>
+    );
 }
