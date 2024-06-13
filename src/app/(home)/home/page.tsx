897af724e368a98d35e4fbe7a7ad7007aec147cd
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-13 11:08:05 +0700

    feat: implement time picker filter

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -1,11 +1,13 @@
 import SportFieldInfoCard from '@/components/sport-field/SportFieldInfoCard';
 import { sportField } from '@/mocks/sport-fields';
 import NearestFields from './components/NearestFields';
+import CustomTimePicker from '@/components/filter/CustomTimePicker';
 
 const HomePage = () => {
     const sportFields = Array(7).fill(sportField);
     return (
         <div>
+            <CustomTimePicker />
             <NearestFields sportFields={sportFields} />
         </div>
     );
