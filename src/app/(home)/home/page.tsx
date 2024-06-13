3cfb812d2b2481c457dfb9cdabf7499c701be11d
zdn-triet-nguyen <triet.nguyen@zodinet.tech>
2024-06-13 16:59:20 +0700

    fix: fix path

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -4,7 +4,7 @@ import CustomTimePicker from "@/components/filter/CustomTimePicker";
 import Banner from "./components/Banner/Banner";
 import PopularPlaces from "./components/popular/PopularPlaces";
 import NearestFields from "./components/nearest/NearestFields";
-import SportFieldsByTime from "./components/fileds-by-time/SportFieldsbyTime";
+import SportFieldsByTime from "./components/fileds-by-time/SportFieldsByTime";
 
 const HomePage = () => {
   const sportFields = Array(12).fill(sportField);
