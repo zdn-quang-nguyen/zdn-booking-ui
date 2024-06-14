d576dc57f60d9b55d13198a0edbb20d585f34268
zdn-triet-nguyen <triet.nguyen@zodinet.tech>
2024-06-14 15:59:18 +0700

    refactor: Remove unused dotenv import in upload-avatar service

--- src/services/firebase/upload-avatar.ts
+++ src/services/firebase/upload-avatar.ts
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 export const uploadImage = async (file: File) => {
   const formData = new FormData();
