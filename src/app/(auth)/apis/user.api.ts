fe542ee1cd6f6892afe1ca2727c166667d772381
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-14 15:23:58 +0700

    feat: set up next auth
    * feat: add session after login
    
    * feat: add private route

--- src/app/(auth)/apis/user.api.ts
+++ src/app/(auth)/apis/user.api.ts
@@ -1,4 +1,23 @@
-class UserApi {}
+'use server';
 
-const userApi = new UserApi();
-export default userApi;
+import { cookies } from 'next/headers';
+
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://127.0.0.1:3000';
+
+export const getUserProfile = async () => {
+  const accessToken = cookies().get('access_token')?.value;
+  const res = await fetch(`${API_HOST}/v1/user`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  });
+  const json = await res.json();
+  if (!res.ok) {
+    throw new Error(json.message);
+  }
+
+  return json.data;
+};
