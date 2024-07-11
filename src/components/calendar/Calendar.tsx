412fb976cb63b574f12cac4ba3f6236d6c3823ea
zdn-duc-nguyen <duc.nguyen@zodinet.tech>
2024-07-11 15:51:11 +0700

    Feat/filter sport filed (#60)
    * feat:add filter by time ui
    
    * feat: filter by time ui
    
    * fix : change  router in pagination
    
    * feat: add calendar sport field base on  booking
    
    * feat: add QR booking
    
    * refactor: add auth into updateQrBooking function
    
    * refactor : add padding into button in management sport filed
    
    * feat: add calendar into home page

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,8 +1,11 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import RowCalendar from './RowCalendar';
 import { getBookingEmptySportField } from '@/libs/api/sport-field-server.api';
 import TimeRow from './TimeRow';
+import { Spin } from 'antd';
+import { cn } from '@/libs/utils';
+import s from '../calendar/calendar.module.scss';
 interface TimeData {
   startTime: string;
   endTime: string;
@@ -10,57 +13,85 @@ interface TimeData {
 }
 export default function Calendar({ sportField }: { sportField: SportField }) {
   const [timeSlots, setTimeSlots] = useState<TimeData[][]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const fetchBookingEmpty = async () => {
+    setIsLoading(true);
+    const today = new Date();
+    console.log(today);
+    const day = today.getDay();
+    const diff = today.getDate() - day + (day === 0 ? -6 : 1);
+    console.log(diff);
+    const monday = new Date(today.setDate(diff));
+    const sunday = new Date(monday);
+    sunday.setDate(monday.getDate() + 6);
 
-  // Promise.all(
-  //   times.map((time) => fetchBookingEmpty(time.startTime, time.endTime)),
-  // ).then((results) => {
-  //   setTimeSlots(results);
-  // });
+    const res = await getBookingEmptySportField({
+      sportFieldId: sportField.id,
+      startTime: today.toISOString(),
+      endTime: sunday.toISOString(),
+      startTimeDay: sportField.startTime,
+      endTimeDay: sportField.endTime,
+    });
+    setTimeSlots(res);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchBookingEmpty();
+  }, []);
 
   return (
-    <div className="relative h-card w-full overflow-x-hidden overflow-y-scroll rounded-large bg-accent-100 md:overflow-x-scroll">
-      <div className="sticky top-0 w-full">
-        <p className="left-6 pl-6 pt-6 text-lg font-bold">Lịch tuần này</p>
-        <div className="top-0 ml-2 mt-7 flex items-center gap-4 px-6">
-          <p className="pointer-events-none h-5 w-12 opacity-0">7:00</p>
-          <div className="h-5 flex-1">
-            <div className="h-5 w-6 text-center">T2</div>
-          </div>
-          <div className="h-5 flex-1">
-            <div className="w-6 text-center">T3</div>
-          </div>
-          <div className="h-5 flex-1">
-            <div className="w-6 text-center">T4</div>
-          </div>
-          <div className="h-5 flex-1">
-            <div className="w-6 text-center">T5</div>
-          </div>
-          <div className="h-5 flex-1">
-            <div className="w-6 text-center">T6</div>
-          </div>
-          <div className="h-5 flex-1">
-            <div className="w-6 text-center">T7</div>
-          </div>
-          <div className="h-5 flex-1">
-            <div className="w-6 text-center">CN</div>
-          </div>
+    <div className={cn(s.main)}>
+      {isLoading ? (
+        <div className="flex h-card w-full items-center justify-center rounded-large bg-accent-100">
+          <Spin size="large" />
         </div>
-      </div>
-      <div className="flex flex-col gap-4 px-6">
-        <div className="flex gap-4">
-          <TimeRow
-            startTime={'2024-06-24T21:00:00.000Z'}
-            endTime={'2024-06-25T14:00:00.000Z'}
-          />
-          <div className="flex gap-4">
+      ) : (
+        <div className="relative h-card w-full overflow-x-hidden overflow-y-scroll rounded-large bg-accent-100 md:overflow-x-scroll">
+          <div className="w-full">
+            <div className="sticky top-0 w-full bg-accent-100">
+              <p className="left-6 pl-6 pt-6 text-lg font-bold">
+                Lịch tuần này
+              </p>
+              <div className="top-0 mt-7 flex items-center gap-4 px-6">
+                <p className="pointer-events-none h-6 w-11 opacity-0">07:00</p>
+                <div className="h-5">
+                  <div className="h-5 w-6 text-center">T2</div>
+                </div>
+                <div className="h-5">
+                  <div className="w-6 text-center">T3 </div>
+                </div>
+                <div className="h-5">
+                  <div className="w-6 text-center">T4</div>
+                </div>
+                <div className="h-5">
+                  <div className="w-6 text-center">T5</div>
+                </div>
+                <div className="h-5">
+                  <div className="w-6 text-center">T6</div>
+                </div>
+                <div className="h-5">
+                  <div className="w-6 text-center">T7</div>
+                </div>
+                <div className="h-5">
+                  <div className="w-6 text-center">CN</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 px-6">
             <div className="flex gap-4">
+              <TimeRow
+                startTime={sportField.startTime}
+                endTime={sportField.endTime}
+              />
+
               {timeSlots.map((slots, index) => (
                 <RowCalendar key={index} timeSlots={slots} />
               ))}
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
